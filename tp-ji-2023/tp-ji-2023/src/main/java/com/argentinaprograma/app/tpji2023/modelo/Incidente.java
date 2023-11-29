93009ea122e376b5c2f00702f8d63ef6cd9849a4
louisizentesano <louisizentesano@gmail.com>
2023-11-29 14:34:39 -0300

    CONSIGNAS 2DA ENTREGA EN CLASE MODELO INCIDENTE

--- tp-ji-2023/tp-ji-2023/src/main/java/com/argentinaprograma/app/tpji2023/modelo/Incidente.java
+++ tp-ji-2023/tp-ji-2023/src/main/java/com/argentinaprograma/app/tpji2023/modelo/Incidente.java
@@ -8,9 +8,11 @@ import java.util.List;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.text.SimpleDateFormat;
 
 @Entity
 @AllArgsConstructor
@@ -19,15 +21,15 @@ import java.util.stream.Collectors;
 public class Incidente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id_incidente;
-    private int id_contrato;
-    private int id_tipo_de_problema;
-    private int id_tecnico;
-    private Date fecha_creacion;
-    private Date fecha_inicio;
-    private Date fecha_fin_estimada;
-    private Duration duracion_estimada; // Duración estimada como objeto Duration
+    @Column(name="id_incidente")
+    private int idIncidente;
+    private int idContrato;
+    private int idTipodeProblema;
+    private int idTecnico;
+    private Date fechaCreacion;
+    private Date fechaInicio;
+    private Date fechaFinEstimada;
+    private Duration duracionEstimada; // Duración estimada como objeto Duration
     private String estado;
 
     @ManyToOne
@@ -45,45 +47,45 @@ public class Incidente {
     @JoinColumn(name = "id_detalle_servicio")
     private DetalleServicio detalleServicio ;
 
-    // Constructor vacío
+    // Constructor vacio
     public Incidente() {
-        this.fecha_creacion = new Date();
+        this.fechaCreacion = new Date();
     }
 
-    public void setId_incidente(int id_incidente) {
-        this.id_incidente = id_incidente;
+    public void setId_incidente(int idIncidente) {
+        this.idIncidente = idIncidente;
     }
 
-    public void setId_contrato(int id_contrato) {
-        this.id_contrato = id_contrato;
+    public void setId_contrato(int idContrato) {
+        this.idContrato = idContrato;
     }
 
-    public void setId_tipo_de_problema(int id_tipo_de_problema) {
-        this.id_tipo_de_problema = id_tipo_de_problema;
+    public void setId_tipo_de_problema(int idTipodeProblema) {
+        this.idTipodeProblema = idTipodeProblema;
     }
 
-    public void setId_tecnico(int id_tecnico) {
-        this.id_tecnico = id_tecnico;
+    public void setId_tecnico(int idTecnico) {
+        this.idTecnico = idTecnico;
     }
 
-    public void setFecha_creacion(Date fecha_creacion) {
-        this.fecha_creacion = fecha_creacion;
+    public void setFecha_creacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
     }
 
-    public void setFecha_inicio(Date fecha_inicio) {
-        this.fecha_inicio = fecha_inicio;
+    public void setFecha_inicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
     }
 
-    public void setFecha_fin_estimada(Date fecha_fin_estimada) {
-        this.fecha_fin_estimada = fecha_fin_estimada;
+    public void setFecha_fin_estimada(Date fechaFinEstimada) {
+        this.fechaFinEstimada = fechaFinEstimada;
     }
 
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    public void setDuracion_estimada(Duration duracion_estimada) {
-        this.duracion_estimada = duracion_estimada;
+    public void setDuracion_estimada(Duration duracionEstimada) {
+        this.duracionEstimada = duracionEstimada;
     }
 
 
@@ -98,13 +100,13 @@ public class Incidente {
 
         if (ultimoIncidente == null) {
             // Si el técnico no tiene incidentes asignados, la fecha de inicio es la fecha de creación
-            this.fecha_inicio = this.fecha_creacion;
+            this.fechaInicio = this.fechaCreacion;
         } else {
             // Si el técnico tiene incidentes asignados, calcular un día después de la fecha_fin_estimada del último incidente
             Calendar calendar = Calendar.getInstance();
-            calendar.setTime(ultimoIncidente.getFecha_fin_estimada());
+            calendar.setTime(ultimoIncidente.getFechaFinEstimada());
             calendar.add(Calendar.DATE, 1); // Añadir un día
-            this.fecha_inicio = calendar.getTime();
+            this.fechaInicio = calendar.getTime();
         }
     }
 
@@ -113,9 +115,9 @@ public class Incidente {
         Incidente incidenteMasTardio = null;
 
         for (Incidente incidente : incidentesAsignadosAlTecnico) {
-            if (incidente.getFecha_fin_estimada() != null && !incidente.getEstado().equalsIgnoreCase("resuelto")) {
+            if (incidente.getFechaFinEstimada() != null && !incidente.getEstado().equalsIgnoreCase("resuelto")) {
                 // Si es el primer incidente que estamos revisando o si la fecha_fin_estimada es más lejana que la del incidente más tardío registrado
-                if (incidenteMasTardio == null || incidente.getFecha_fin_estimada().after(incidenteMasTardio.getFecha_fin_estimada())) {
+                if (incidenteMasTardio == null || incidente.getFechaFinEstimada().after(incidenteMasTardio.getFechaFinEstimada())) {
                     incidenteMasTardio = incidente;
                 }
             }
@@ -125,6 +127,7 @@ public class Incidente {
     }
 
     // Método para calcular la duración estimada del incidente
+    // sumando las duraciones de los problemas en una colección de problemas
     public Duration calcularDuracionEstimada() {
         Duration duracionTotal = Duration.ZERO;
         for (Problema problema : problemas) {
@@ -138,11 +141,11 @@ public class Incidente {
         Date fechaActual = new Date();
         boolean todosProblemasResueltos = problemas.stream().allMatch(problema -> problema.getEstado().equalsIgnoreCase("finalizado"));
 
-        if (fecha_inicio != null && fecha_inicio.after(fechaActual)) {
+        if (fechaInicio != null && fechaInicio.after(fechaActual)) {
             return "pendiente";
-        } else if (fecha_inicio != null && fecha_inicio.before(fechaActual) && (fecha_fin_estimada == null || fecha_fin_estimada.after(fechaActual))) {
+        } else if (fechaInicio != null && fechaInicio.before(fechaActual) && (fechaFinEstimada == null || fechaFinEstimada.after(fechaActual))) {
             return "en proceso";
-        } else if (fecha_fin_estimada != null && fecha_fin_estimada.before(fechaActual) && !todosProblemasResueltos) {
+        } else if (fechaFinEstimada != null && fechaFinEstimada.before(fechaActual) && !todosProblemasResueltos) {
             return "demorado";
         } else if (todosProblemasResueltos) {
             return "resuelto";
@@ -166,42 +169,54 @@ public class Incidente {
                     }
                 }
             }
-
             return fechaCierreMasReciente;
         }
-
-        return null; // Si no se cumplen las condiciones, devuelve null
+        return null; 
     }
-    public Date obtenerFechaFinEstimada() {
-        if (fecha_inicio != null && duracion_estimada != null) {
+    
+    
+    //Devuelve una fecha calculada basada en una fecha de inicio y una duración estimada
+    public Date obtenerFechaFinEstimada(int dias) {
+        if (fechaInicio != null && duracionEstimada != null) {
             Calendar calendar = Calendar.getInstance();
-            calendar.setTime(fecha_inicio);
-            calendar.add(Calendar.HOUR, (int)duracion_estimada.toHours()); // Agregar la duración estimada en horas
-
+            calendar.setTime(fechaInicio);
+            calendar.add(Calendar.HOUR, (int)duracionEstimada.toHours()); // Agregar la duración estimada en horas
             return calendar.getTime();
         }
-
         return null; // Si no se cumplen las condiciones, devuelve null
     }
+    
 
     // Método de notificación al cliente al crear el incidente
     public void notificarClienteFechaPosibleSolucion(Incidente incidente) {
-
+    	Date fechaPosibleResolucion = this.getFechaFinEstimada();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        System.out.println("Incidente creado. Fecha posible de resolución: " + sdf.format(fechaPosibleResolucion));
     }
 
-    // Método para obtener la fecha posible de resolución (puedes personalizar cómo se calcula)
-    private Date obtenerFechaPosibleResolucion(Incidente incidente) {
-        // Aquí calcula la fecha posible de resolución
-        // FALTA CODIGO
-        return new Date();
-    }
 
     public void notificarTecnico(Incidente incidente) {
+    	System.out.println("Notificar al técnico sobre el incidente " + incidente.getIdIncidente());
 
     }
 
-    public void notificarClienteEstadoResulto(Incidente incidente) {
+    public void notificarClienteEstadoResuelto(Incidente incidente) {
+    	String estado = calcularEstado();
 
+        if ("pendiente".equalsIgnoreCase(estado) || "en proceso".equalsIgnoreCase(estado) || "demorado".equalsIgnoreCase(estado)) {
+            notificarTecnico(incidente);
+            // Luego, notifica al cliente sobre el estado y la fecha estimada
+            notificarClienteFechaPosibleSolucion(incidente);
+        } else if ("resuelto".equalsIgnoreCase(estado)) {
+            // El incidente ya está resuelto, envía un mensaje específico
+            enviarMensajeResuelto();
+        }
+    }
+    
+    private void enviarMensajeResuelto() {
+        // para notificar al cliente que el incidente está resuelto
+        System.out.println("El incidente ha sido resuelto. Gracias por su paciencia.");
+ 
     }
 
     // Método para obtener incidentes por fecha en una lista dada
@@ -209,7 +224,7 @@ public class Incidente {
         List<Incidente> incidentesPorFecha = new ArrayList<>();
 
         for (Incidente incidente : listaIncidentes) {
-            if (incidente.getFecha_creacion() != null && incidente.getFecha_creacion().equals(fecha)) {
+            if (incidente.getFechaCreacion() != null && incidente.getFechaCreacion().equals(fecha)) {
                 incidentesPorFecha.add(incidente);
             }
         }
@@ -220,20 +235,63 @@ public class Incidente {
 
     // Método para obtener el técnico con más incidentes resueltos en los últimos N días
     public Tecnico obtenerTecnicoMasIncidentesResueltos(int dias, List<Incidente> listaIncidentes) {
-        //FALTA CODIGO
-        return null;
+    	// Obtener la fecha actual y calcular la fecha límite hace N días
+        Date fechaLimite = obtenerFechaFinEstimada(dias);
 
+        // Filtar los incidentes resueltos en el rango de días especificado
+        List<Incidente> incidentesResueltos = listaIncidentes.stream()
+                .filter(incidente -> "resuelto".equalsIgnoreCase(incidente.calcularEstado()) && incidente.getFechaCreacion().after(fechaLimite))
+                .collect(Collectors.toList());
+
+        // Contar la cantidad de incidentes resueltos por cada técnico
+        Map<Tecnico, Long> conteoPorTecnico = incidentesResueltos.stream()
+                .collect(Collectors.groupingBy(Incidente::getTecnico, Collectors.counting()));
+
+        // Encontrar al técnico con más incidentes resueltos
+        Optional<Map.Entry<Tecnico, Long>> tecnicoMasIncidentes = conteoPorTecnico.entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        // Devolver al técnico con más incidentes resueltos
+        return tecnicoMasIncidentes.map(Map.Entry::getKey).orElse(null);
     }
-    // Método que devuelve el técnico con más incidentes resueltos de una determinada especialidad en los últimos N días
+    
+    
     public Tecnico obtenerTecnicoMasIncidentesResueltosEspecialidad(int dias, int idEspecialidad, List<Incidente> listaIncidentes) {
-        //FALTA CODIGO
-        return null;
+        // Obtener la fecha actual y calcular la fecha límite hace N días
+        Date fechaLimite = obtenerFechaFinEstimada(dias);
+
+        // Filtar los incidentes resueltos en el rango de días especificado y de la especialidad dada
+        List<Incidente> incidentesResueltosEspecialidad = listaIncidentes.stream()
+                .filter(incidente -> "resuelto".equalsIgnoreCase(incidente.calcularEstado()) &&
+                        incidente.getFechaCreacion().after(fechaLimite) &&
+                        incidente.getTecnico().getEspecialidades().stream().anyMatch(especialidad -> especialidad.getIdEspecialidad() == idEspecialidad))
+                .collect(Collectors.toList());
+
+        // Contar la cantidad de incidentes resueltos por cada técnico
+        Map<Tecnico, Long> conteoPorTecnico = incidentesResueltosEspecialidad.stream()
+                .collect(Collectors.groupingBy(Incidente::getTecnico, Collectors.counting()));
+
+        // Encontrar al técnico con más incidentes resueltos
+        Optional<Map.Entry<Tecnico, Long>> tecnicoMasIncidentes = conteoPorTecnico.entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        // Devolver al técnico con más incidentes resueltos
+        return tecnicoMasIncidentes.map(Map.Entry::getKey).orElse(null);
     }
 
+    
+  
     public Tecnico obtenerTecnicoMasRapido(List<Incidente> incidentes) {
-        //FALTA CODIGO
-        return null;
+        // Ordenar los incidentes por duración estimada en orden ascendente
+        List<Incidente> incidentesOrdenados = incidentes.stream()
+                .filter(incidente -> "resuelto".equalsIgnoreCase(incidente.calcularEstado()))
+                .sorted(Comparator.comparing(Incidente::calcularDuracionEstimada))
+                .collect(Collectors.toList());
+
+        // Obtener al técnico asociado al primer incidente (el más rápido)
+        return incidentesOrdenados.isEmpty() ? null : incidentesOrdenados.get(0).getTecnico();
     }
+
 
 }
 
